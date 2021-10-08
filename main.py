ad5fb3c0806659b2e8ac829b91ce3710ae2a1638
Geoffrey Phelan <gphelan@hotmail.co.uk>
2021-10-08 10:10:24 +0200

    uncomment db.create_all

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ class Comment(db.Model):
     post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
     parent_post = relationship("BlogPost", back_populates="comments")
 
-#db.create_all()
+db.create_all()
 
 
 # Create admin only decorator function
